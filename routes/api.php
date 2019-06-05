8bda18bd733bca1c4f6fbb11cff2e13196dc0fc6
夏晓豪 <863976951@qq.com>
2019-06-05 11:25:10 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,37 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function ($api) {
+
+});
+
+$api->version(['v1', 'v2'], function ($api) {
+
+});
+
+$api->version('v1', ['middleware' => 'foo'], function ($api) {
+
+});
+
+$api->version('v1', function ($api) {
+    $api->get('users/{id}', 'App\Api\Controllers\UserController@show');
+});
+
+$api->version('v1', function ($api) {
+    $api->get('users/{id}', 'App\Api\V1\Controllers\UserController@show');
+});
+
+$api->version('v2', function ($api) {
+    $api->get('users/{id}', 'App\Api\V2\Controllers\UserController@show');
+});
+
+$api->get('users/{id}', ['as' => 'users.index', 'uses' => 'Api\V1\UserController@show']);
+
+app('Dingo\Api\Routing\UrlGenerator')->version('v1')->route('users.index');
